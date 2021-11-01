e34a622bc61ce166bf55fc3ada4d3558d76754e7
siloob <siloob@protonmail.com>
2021-11-01 21:43:20 +0100

    add csv into db

--- snepdata/admin.py
+++ snepdata/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from snepdata.models import Certification
 
-# Register your models here.
+
+class CertificationAdmin(admin.ModelAdmin):
+    search_fields = ['artist','title','release_date','category','certification_type','certification_date']
+    ordering = ('-certification_date',)
+
+    class Meta:
+        Model = Certification
+
+admin.site.register(Certification, CertificationAdmin)
