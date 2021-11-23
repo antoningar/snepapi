7498881c5bea1ab1a5056e6d2e60df8c41fe222b
siloob <siloob@protonmail.com>
2021-11-24 00:53:10 +0100

    add test base api

---
+++ snepdata/test/api_test.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from unittest import mock
+from rest_framework.test import APIRequestFactory, APIClient
+
+import json
+
+from snepdata.models import Certification
+
+class APITestCase(TestCase):
+    def setUp(self) -> None:
+        self.certification = Certification(artist='artiste', category='category', certification_type='OR')
+        self.certification.save()
+        self.APIClient =  APIClient()
+
+    def test_cetifications(self):
+        request = self.APIClient.get('/api/certifications', format='json')
+        response = json.loads(request.content)
+
+        self.assertEqual(request.status_code, 200)
+        self.assertEqual(len(response['results']), 1)
+        self.assertEqual(response['results'][0]['artist'], self.certification.artist)
+        self.assertEqual(response['results'][0]['category'], self.certification.category)
